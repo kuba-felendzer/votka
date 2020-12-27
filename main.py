a38b34177c33409980a79c42b4a1ab34fe4a44ae
Kuba Felendzer <66078440+kuba-felendzer@users.noreply.github.com>
2020-12-27 07:32:24 -0600

    Added comments and a couple tweaks

--- main.py
+++ main.py
@@ -2,80 +2,77 @@ import discord
 from discord.ext import commands
 from datetime import datetime
 
-f = open("TOKEN", "r")
-token = f.read()
+f = open("TOKEN", "r") # open the TOKEN file
+token = f.read() # read all the data from it and assign it to token
 
-z_votes = 0
-l_votes = 0
-t_votes = 0
-r_votes = 0
-
-uv = []
-voting = False
+r_votes = 0 # total amount of votes for robot
+z_votes = 0 # total amount of votes for zorro
+l_votes = 0 # total amount of votes for lobby
+t_votes = 0 # total amount of votes
 
 
-bot = commands.Bot(command_prefix=":")
+uv = [] # list of users who have voted
 
+startup_time = datetime.utcnow() # the UTC time when this script starts
 
-# @bot.event
-# async def on_message(message):
-    # if voting:
-        # if
-    # pass
+bot = commands.Bot(command_prefix=":") # creates the actual bot with the prefix of :
 
-@bot.command(name="v")
-async def v(ctx, choice):
-    global z_votes, l_votes, t_votes, r_votes
-    voting = True
-    a = ctx.author
-    vlc = bot.get_channel(792637369600966676)
-    if a in uv:
-        await a.send("you already voted, silly!")
-        return
+@bot.event
+async def on_ready(): # called once when the bot starts up
+    print("Votka ready!") # print a message to console, lets you know when the bot is on
+    print(startup_time.strftime("%c")) # prints the time it started up
+
+@bot.command(name="v") # creates a command of the name v
+async def v(ctx, choice): # voting command, usage :v <choice>, which in this case is 1, 2, or 3
+    global z_votes, l_votes, t_votes, r_votes # global these variables to make sure I'm using the right ones
+    a = ctx.author # turns "ctx.author.send" to "a.send"
+    vlc = bot.get_channel(792637369600966676) # the specific channel that the voting data ("Someone voted for Lobby!")
+    if a in uv: # if the command author (a) is in the list of users who have voted
+        await a.send("you already voted, silly!") # send them a message telling them they already voted
+        return # return nothing, skip the rest of the function
     
-    if choice in "123":
-        uv.append(a)
-        if choice == "1": 
-            await a.send("Logged your vote for **Zorro**.")
-            await vlc.send("Someone voted for **Zorro**!")
-            z_votes += 1
-            t_votes += 1
-        elif choice == "2": 
-            await a.send("Logged your vote for **Lobby**.")
-            await vlc.send("Someone voted for **Lobby**!")
-            l_votes += 1
-            t_votes += 1
-        elif choice == "3": 
-            await a.send("Logged your vote for **Robot**.")
-            await vlc.send("Someone voted for **Robot**!")
-            r_votes += 1
-            t_votes += 1
-
-        
-    else:
-        await a.send("That is not a valid option!")
-        return
+    if choice in ["1", "2", "3"]: # if the choice is either a one, two, or three (correct options)
+        uv.append(a) # add this person to the list of people who have voted
+        if choice == "1": # if they chose 1 (Zorro)
+            await a.send("Logged your vote for **Zorro**.") # tell them their vote is counted
+            await vlc.send("Someone voted for **Zorro**!") # tell the #votes channel someone voted for zorro
+            z_votes += 1 # add one to zorro's votes
+        elif choice == "1": # if they chose 2 (Lobby)
+            await a.send("Logged your vote for **Lobby**.") # tell them their vote is counted
+            await vlc.send("Someone voted for **Lobby**!") # tell the #votes channel someone voted for lobby
+            l_votes += 1 # add one to lobby's votes
+        elif choice == "3": # if they chose 3 (Robot)
+            await a.send("Logged your vote for **Robot**.") # tell them their vote is counted
+            await vlc.send("Someone voted for **Robot**!") # tell the #votes channel someone voted for robot
+            r_votes += 1 # add one to robot's votes
+            
+        t_votes += 1 # add one to the total vote, since this is valid vote  
+    else: # if the person did not cast a 1, 2, or 3
+        await a.send("That is not a valid option!") # tell them they can't pick that candidate
+        return # exit the function
 
 
-@bot.command(name="d")
-async def d(ctx):
-    voting_embed = discord.Embed(title="Director Election", description="Elect our new director!", color=0x00ff00)
-    voting_embed.add_field(name="Choice 1: ", value="Zorro")
-    voting_embed.add_field(name="Choice 2: ", value="Lobby")
-    voting_embed.add_field(name="Choice 3: ", value="Robot")
+@bot.command(name="d") # creates a command of the name d
+async def d(ctx): # display command, usage :d
+    voting_embed = discord.Embed(title="Director Election", description="Elect our new director!", color=10494192) # create the embed
+    voting_embed.add_field(name="Choice 1: ", value="Zorro, `:v 1`") # add zorro
+    voting_embed.add_field(name="Choice 2: ", value="Lobby, `:v 2`") # add lobby
+    voting_embed.add_field(name="Choice 3: ", value="Robot, `v: 3`") # add robot
+    # this function just displays who is running and how to vote for them
+    await ctx.send(embed=voting_embed) # send the embed in the chat that requested it
 
-    await ctx.send(embed=voting_embed)
+@bot.command(name="t") # creates a command of the name t
+async def t(ctx): # total command, usage ;t
+    global z_votes, l_votes, t_votes, r_votes, startup_time # make sure I am using the right vars
+    time = datetime.utcnow() # the time when this command is called
+    # https://strftime.org/ formatting data from here
+    e = discord.Embed(title="Votes Tally", description=f"As of {time.strftime('%c')} UTC\nLast restarted on {startup_time.strftime('%c')} UTC", color=10494192) # create the embed
+    # time.strftime("%c") tells python to represent the time according to the "c" formatting option, same for startup_time.strftime
+    e.add_field(name="Votes for Zorro: ", value=str(z_votes), inline=False) # add zorro's votes
+    e.add_field(name="Votes for Lobby: ", value=str(l_votes), inline=False) # add lobby's votes
+    e.add_field(name="Votes for Robot: ", value=str(r_votes), inline=False) # add robot's votes
+    e.add_field(name="Total Votes: ", value=str(t_votes), inline=False) # add the total votes
 
-@bot.command(name="t")
-async def t(ctx):
-    global z_votes, l_votes, t_votes, r_votes
-    time = datetime.utcnow()
-    e = discord.Embed(title="Votes Tally", description=f"As of UTC {time}")
-    e.add_field(name="Votes for Zorro: ", value=str(z_votes), inline=False)
-    e.add_field(name="Votes for Lobby: ", value=str(l_votes), inline=False)
-    e.add_field(name="Votes for Robot: ", value=str(r_votes), inline=False)
-    e.add_field(name="Total Votes: ", value=str(t_votes), inline=False)
+    await ctx.send(embed=e) # send the table
 
-    await ctx.send(embed=e)
-
-bot.run(token)
+bot.run(token) # allows the bot to run and function
